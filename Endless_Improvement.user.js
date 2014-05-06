7cb0fa76a150a66bf11e46e94b667e763cc4369f
feildmaster <admin@feildmaster.com>
2014-05-06 18:16:16 -0500

    Add ability to auto sell items Complete with ability to enable/disable through options page.

--- Endless_Improvement.user.js
+++ Endless_Improvement.user.js
@@ -6,3 +6,43 @@
 // @version     1pre
 // @grant       none
 // ==/UserScript==
+
+// Start "Selling Inventory Items"
+game.inventory.sell = function sell(item) {
+    // Get the sell value and give the gold to the player; don't use the gainGold function as it will include gold gain bonuses
+    var value = item.sellValue;
+    game.player.gold += value;
+    // Increase stats!
+    game.stats.itemsSold++;
+    game.stats.goldFromItems += value;
+}
+
+var autoSellLoot = false;
+game.inventory.lootItem = function lootItem(item) {
+    for (var x = 0; x < game.inventory.maxSlots; x++) {
+        if (game.inventory.slots[x] == null) {
+            // You can only sell what you can carry!
+            if (autoSellLoot && !(item.rarity == ItemRarity.LEGENDARY || item.rarity == ItemRarity.EPIC)) {
+                game.inventory.sell(item);
+            } else {
+                game.inventory.slots[x] = item;
+                $("#inventoryItem" + (x + 1)).css('background', ('url("/includes/images/itemSheet2.png") ' + item.iconSourceX + 'px ' + item.iconSourceY + 'px'));
+            }
+            game.stats.itemsLooted++;
+            break;
+        }
+    }
+}
+// End selling items
+
+
+// Start insert script options
+$("#optionsWindowOptionsArea").append('<div id="improvementOptionsTitle" class="optionsWindowOptionsTitle">Endless Improvement Options</div>');
+// Add function to toggle selling ability
+game.autoSellOptionClick = function autoSellOptionClick() {
+    autoSellLoot = !autoSellLoot;
+    $("#autoSellValue").html(autoSellLoot?"ON":"OFF");
+}
+// Add option for auto sell inventory items
+$("#optionsWindowOptionsArea").append('<div class="optionsWindowOption" onmousedown="game.autoSellOptionClick()">Auto sell new (non-rare) loot: <span id="autoSellValue">OFF</span></div>');
+// End insert script options
