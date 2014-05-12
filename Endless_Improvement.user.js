853fe1e58454159ad9cbfac3cbd1825d3a6a9acd
feildmaster <admin@feildmaster.com>
2014-05-12 03:50:19 -0500

    Prep for next release

--- Endless_Improvement.user.js
+++ Endless_Improvement.user.js
@@ -3,7 +3,7 @@
 // @description Script dedicated to improving kruv's endless battle browser game
 // @namespace   http://feildmaster.com/
 // @include     http://www.kruv.net/endlessBattle.html
-// @version     1.2.1
+// @version     1.3pre
 // @updateURL   https://raw.githubusercontent.com/feildmaster/EndlessImprovement/master/Endless_Improvement.meta.js
 // @grant       none
 // ==/UserScript==
