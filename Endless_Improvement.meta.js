6c31d8b0490249a24c6e0c6bd6b8ca5268a9f6f9
feildmaster <admin@feildmaster.com>
2014-05-16 15:45:39 -0500

    Release version 1.4

--- Endless_Improvement.meta.js
+++ Endless_Improvement.meta.js
@@ -4,7 +4,7 @@
 // @description Script dedicated to improving kruv's endless battle browser game
 // @namespace   http://feildmaster.com/
 // @include     http://www.kruv.net/endlessBattle.html
-// @version     1.3.1
+// @version     1.4
 // @updateURL   https://raw.githubusercontent.com/feildmaster/EndlessImprovement/release/Endless_Improvement.meta.js
 // @downloadURL https://raw.githubusercontent.com/feildmaster/EndlessImprovement/release/Endless_Improvement.user.js
 // @source      https://github.com/feildmaster/EndlessImprovement/
